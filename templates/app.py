c246210ce442194e2c37046ef3f5130871cc7f7d
Jonathan <jonathan.olavarria@usm.cl>
2021-07-11 19:58:05 -0400

    m

---
+++ templates/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request
+from db import add
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    print("hola") #add(request.data)
+
+if __name__ == '__mcheain__':
+    app.run(debug = True)

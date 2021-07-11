ea37a5aba993376c7bdb6c44f885a56e48151986
Jonathan <jonathan.olavarria@usm.cl>
2021-07-11 19:25:20 -0400

    db

--- db.py
+++ db.py
@@ -8,10 +8,10 @@ database_url = os.environ['DATABASE_URL']
 
 def get_connection_unsafe():
     return psycopg2.connect(
-                dbname='deg506b08u73i2',
-                user='smtckjvoabcoxs',
-                password='e069b8102d92c09270411baaa566d6a742994ebe43c5ebf778341fcf4494fec1',
-                host='ec2-35-174-127-63.compute-1.amazonaws.com',
+                dbname='de43tdpr0fn1om',
+                user='qwykdhdzervpiv',
+                password='423b948b683cf5c80cf56c651b24c3381a00fffe0f2311ebf6eb3f3b070544ac',
+                host='ec2-54-225-228-142.compute-1.amazonaws.com',
                 port='5432'
             )
 
@@ -32,35 +32,13 @@ def get_connection():
                 port=port
             )
 
-def select_messages():
+def add(data):
     connection = get_connection()
     cursor = connection.cursor()
-    cursor.execute("SET TIMEZONE = 'America/Santiago'")
-    cursor.execute("SELECT * FROM messages")
-    fetch = cursor.fetchall()
-    connection.close()
-    return fetch
-
-
-def add_message(username, message):
-    connection = get_connection()
-    cursor = connection.cursor()
-    query = "INSERT INTO messages (username, message, time) VALUES ('" + username + "', '" + message + "', NOW())"
+    query = "INSERT INTO paypal_data (data) VALUES ('" + data + "')"
     cursor.execute(query)
-    #query = 'INSERT INTO messages(message_id, username, message, time) VALUES (default, %s, %s, NOW())'
-    #cursor.execute(query, ['nick_test', 'message_test'])
     cursor.close()
     connection.commit()
     connection.close()
 
-def delete_message(id):
-    connection = get_connection()
-    cursor = connection.cursor()
-    query = "DELETE FROM messages WHERE message_id = '" + id + "'"
-    cursor.execute(query)
-    #query = "DELETE FROM messages WHERE id = %s"
-    #cursor.execute(query, ['nick_test', 'message_test'])
-    cursor.close()
-    connection.commit()
-    connection.close()
     

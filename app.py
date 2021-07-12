9e1636b6059db0a9f898e5cdab9e2c37e0a562fd
Jonathan <jonathan.olavarria@usm.cl>
2021-07-11 20:54:12 -0400

    m

--- app.py
+++ app.py
@@ -5,22 +5,23 @@ app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
-    cad=""
-    cad+="URL:"+request.url+"<br/>"
-    cad+="Método:"+request.method+"<br/>"
-    cad+="header:<br/>"
-    for item,value in request.headers.items():
-        cad+="{}:{}<br/>".format(item,value)    
-    cad+="información en formularios (POST):<br/>"
-    for item,value in request.form.items():
-        cad+="{}:{}<br/>".format(item,value)
-    cad+="información en URL (GET):<br/>"
-    for item,value in request.args.items():
-        cad+="{}:{}<br/>".format(item,value)    
-    cad+="Ficheros:<br/>"
-    for item,value in request.files.items():
-        cad+="{}:{}<br/>".format(item,value)
-    add(cad)
+    add(request.data.decode("utf-8"))
+    #cad=""
+    #cad+="URL:"+request.url+"<br/>"
+    #cad+="Método:"+request.method+"<br/>"
+    #cad+="header:<br/>"
+    #for item,value in request.headers.items():
+    #    cad+="{}:{}<br/>".format(item,value)    
+    #cad+="información en formularios (POST):<br/>"
+    #for item,value in request.form.items():
+    #    cad+="{}:{}<br/>".format(item,value)
+    #cad+="información en URL (GET):<br/>"
+    #for item,value in request.args.items():
+    #    cad+="{}:{}<br/>".format(item,value)    
+    #cad+="Ficheros:<br/>"
+    #for item,value in request.files.items():
+    #    cad+="{}:{}<br/>".format(item,value)
+    #add(cad)
     return "200"
 
 if __name__ == '__mcheain__':

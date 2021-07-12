2153017c5c88481eb2bc8d8bcdc4a5a348a37114
Jonathan <jonathan.olavarria@usm.cl>
2021-07-11 21:17:58 -0400

    m

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
-    add(request.stream.read().decode("utf-8"))
+    add(request.text)
     #cad=""
     #cad+="URL:"+request.url+"<br/>"
     #cad+="Método:"+request.method+"<br/>"
